6806723c5275ada1a44d999bc1c21a525477f8c2
Thibaut DUMOULIN <tdumouli@e2r3p12.42.fr>
2016-11-23 07:13:05 +0100

    ca marche

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tdumouli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/21 06:21:14 by tdumouli          #+#    #+#             */
-/*   Updated: 2016/11/21 06:21:18 by tdumouli         ###   ########.fr       */
+/*   Updated: 2016/11/23 07:12:28 by tdumouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,35 +53,40 @@ int		remplissage(const int fd, char **line)
 
 int		decoupage(char **line, char **stk)
 {
-	int		start;
+	int		deb;
 	int		end;
 	char	*tmp;
 
-	start = 0;
+	deb = 0;
 	end = 0;
-	while (*(*line + start) != '\n' && *(*line + start) != -1)
-		++start;
-	while (*(*line + start + end) != '\0')
+	while (*(*line + deb) != '\n' && *(*line + deb) && *(*line + deb) != -1)
+		++deb;
+	while (*(*line + deb + end) != -1 && *(*line + deb + end) != '\0')
 		++end;
 	tmp = *line;
-	*stk = ft_strsub(tmp, 0, start);
-	*line = ft_strsub(tmp, start + 1, end - 1);
+	*stk = ft_strsub(tmp, 0, deb);
+	*line = ft_strsub(tmp, deb + 1, end - 1);
 	free(tmp);
 	return (0);
 }
 
 int		get_next_line(const int fd, char **line)
 {
-	static char	*offset;
-	int			ret;
+	static t_list	*lsd;
+	t_list			*offset;
+	int				ret;
 
-	if (!offset)
-		if (!(offset = (char *)malloc(1)))
+	if (!(offset = (ft_lstfindsize(lsd, fd))))
+	{
+		if (!(offset = ft_lstnew("\0", 1)))
 			return (-1);
-	if ((ret = remplissage(fd, &offset)) == -1)
+		offset->content_size = fd;
+		ft_lstadd(&lsd, offset);
+	}
+	if ((ret = remplissage(fd, (char **)&(offset->content))) == -1)
 		return (-1);
-	if (!ret)
+	if (!ret && !*(char *)offset->content)
 		return (0);
-	decoupage(&offset, line);
+	decoupage((char **)&(offset->content), line);
 	return (1);
 }

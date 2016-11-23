b50005d259598641d2c0beed270e2e85ab286bf2
Thibaut DUMOULIN <tdumouli@e2r3p12.42.fr>
2016-11-23 12:03:55 +0100

    passe la norme

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tdumouli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/21 06:21:14 by tdumouli          #+#    #+#             */
-/*   Updated: 2016/11/23 07:12:28 by tdumouli         ###   ########.fr       */
+/*   Updated: 2016/11/23 12:03:10 by tdumouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,30 @@
 #include "libft.h"
 #include <stdlib.h>
 
-int		remplissage(const int fd, char **line)
+int		remplissage(const int fd, char **line, int i, char **buf)
 {
-	char	*buf;
 	char	*tmp;
 	int		rd;
-	int		i;
 	int		flag;
 
-	i = 0;
 	while (*(*line + i) != '\n' && *(*line + i) != -1 && (*((*line) + i)))
 		++i;
 	flag = (*(*line + i) == '\n' || *(*line + i) == -1);
-	if (!(buf = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1))))
-		return (-1);
-	while (!flag && (rd = read(fd, buf, BUFF_SIZE)))
+	while (!flag && (rd = read(fd, *buf, BUFF_SIZE)))
 	{
 		if (rd == -1)
 			return (-1);
-		buf[rd] = '\0';
+		(*buf)[rd] = '\0';
 		i = 0;
-		while (*(buf + i) != '\n' && i != rd)
+		while (*(*buf + i) != '\n' && i != rd)
 			++i;
-		if (*(buf + i) == '\n')
-			flag = 1;
-		else
-			*(buf + i + 1) = '\0';
-		if (!(tmp = ft_strjoin(*line, buf)))
+		if (!(*(*buf + i) == '\n') || !(flag = 1))
+			*(*buf + i + 1) = '\0';
+		if (!(tmp = ft_strjoin(*line, *buf)))
 			return (-1);
 		free(*line);
 		*line = tmp;
 	}
-	free(buf);
 	return (flag);
 }
 
@@ -72,6 +64,7 @@ int		decoupage(char **line, char **stk)
 
 int		get_next_line(const int fd, char **line)
 {
+	char			*buf;
 	static t_list	*lsd;
 	t_list			*offset;
 	int				ret;
@@ -83,8 +76,11 @@ int		get_next_line(const int fd, char **line)
 		offset->content_size = fd;
 		ft_lstadd(&lsd, offset);
 	}
-	if ((ret = remplissage(fd, (char **)&(offset->content))) == -1)
+	if (!(buf = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1))))
 		return (-1);
+	if ((ret = remplissage(fd, (char **)&(offset->content), 0, &buf)) == -1)
+		return (-1);
+	free(buf);
 	if (!ret && !*(char *)offset->content)
 		return (0);
 	decoupage((char **)&(offset->content), line);
